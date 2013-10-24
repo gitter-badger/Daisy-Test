c528cac2eadde21710cfca2594bfa3f2483cfa0a
Daisy Olsen <daisy@copyblogger.com>
2013-10-24 19:39:25 -0400

    Test Commit

---
+++ 2.php
@@ -0,0 +1,22 @@
+If you’re an online blogger, web designer/developer, or website owner looking to generate income and help take WordPress to a higher level by promoting products for the largest and fastest growing online blogging and website platform then our affiliate program is perfect for you.
+
+Before applying to our affiliate program, please read the following:
+
+Affiliate FAQs
+Affiliate Guidelines
+Affiliate Payments
+Sign up is easy and takes just a few simple steps.
+
+1. Apply
+Sign-up for a ShareASale affiliate account and apply for our Affiliate program in one step by filling out the application here. (If you already have a ShareASale account use the same link to apply to our program.)
+
+2. Acceptance
+Once you receive your approval email from us, welcoming you to the StudioPress Affiliate Program, review our Program Terms & Conditions via your SAS dashboard.
+
+3. Linking and Banners
+You can link to us using text links or banners. Once you have selected a product you want to promote you can then copy and paste the code into your website or blog. Find our creatives here including all banners and text links. (Requires a ShareASale account.)
+
+4. Community and Blog
+We want to work closely with our Affiliates so once you’re all signed up join us to discuss tips and tricks to help you make more money and stay up to date on the latest promotions by following our Twitter and Facebook updates.
+
+Thanks so much for supporting StudioPress and feel free to contact us should you have any questions.
